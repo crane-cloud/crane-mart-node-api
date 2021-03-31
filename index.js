2ed4f5fac716107b155220b30b7b36c1139248e6
Kawere Wagaba <ckwagaba@gmail.com>
2021-03-31 14:17:20 +0300

    connect to db

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '196.43.134.154',
+  port: '32765',
+  user: 'RnExjCEdqStFhfvJ',
+  password: '!bcS[[PBVN:4~nL1f9fWu(E,xQD_64GC',
+  database: 'GjYAzYWSwHQJLCHxKopreHkJ'
+});
+
+connection.connect(error => {
+  if (error) {
+    console.error(`Error connecting: ${error.stack}`);
+
+    return;
+  }
+
+  console.log(`Connected as id: ${connection.threadId}`);
+});
+
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to Crane Mart');
 });
